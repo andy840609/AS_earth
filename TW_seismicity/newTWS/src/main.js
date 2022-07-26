d8cab7dc3c5dd148b39bc7521018057efb8f2f10
andy <andy@localhost.localdomain>
2022-07-26 17:00:01 +0800

    07/26/2022

--- TW_seismicity/newTWS/src/main.js
+++ TW_seismicity/newTWS/src/main.js
@@ -51,10 +51,10 @@ function TWSanime() {
     };
 
     async function chart() {
-        const chartContainerJQ = $(selector);
+        const selectorD3 = d3.select(selector);
 
         function init() {
-            chartContainerJQ.append(`
+            document.querySelector(selector).insertAdjacentHTML('beforeend', `
             <div id="form-chart">
                 <div class="form-group" id="chartsOptions" style="display: inline;">
                     <div class="row">
@@ -65,7 +65,8 @@ function TWSanime() {
 
                 <div class="form-group"  id="chartMain">
 
-                    <div id="Map"></div>          
+                    <div id="Map">
+                    </div>          
                 
                     <div id="outerdiv"
                         style="position:fixed;top:0;left:0;background:rgba(0,0,0,0.7);z-index:10;width:100%;height:100%;display:none;">
@@ -80,255 +81,14 @@ function TWSanime() {
                     </div>
                 </div> 
             </div>
-                `);
+            `);
 
         };
         async function printChart() {
-            chartContainerJQ.find('#Map').children().remove();
-
-            let getChartMenu = () => {
-                // console.log(d.data);
-                let div = document.createElement("div");
-                div.setAttribute("id", "chart" + i);
-                div.setAttribute("class", "chart col-md-12 col-sm-12");
-                div.setAttribute("style", "position:relative");
-
-                let nav = document.createElement('nav');
-                nav.setAttribute("id", "nav" + i);
-                nav.setAttribute("class", "toggle-menu");
-                nav.setAttribute("style", "position:absolute");
-                nav.style.right = "0";
-
-                let a = document.createElement('a');
-                a.setAttribute("class", "toggle-nav");
-                a.setAttribute("href", "#");
-                a.innerHTML = "&#9776;";
-                nav.append(a);
-
-                let ul = document.createElement("ul");
-                ul.classList.add("active");
-                nav.append(ul);
-
-                let chartDropDown = ['bigimg', 'svg', 'png', 'jpg'];
-                chartDropDown.forEach(option => {
-                    let li = document.createElement("li");
-                    let item = document.createElement("a");
-                    item.href = "javascript:void(0)";
-
-                    if (option != chartDropDown[0])
-                        item.innerHTML = "下載圖表爲" + option;
-                    else
-                        item.innerHTML = "檢視圖片";
-
-                    item.addEventListener("click", (e, a) => {
-                        let svgArr = [];
-                        let svg = chartContainerJQ.find("#" + $(e.target).parents('.chart')[0].id).children('svg')[0];
-                        svgArr.push(svg);
-                        let xAxisName = document.querySelector('input[name ="xAxisName"]:checked').value;
-                        let xAxisScale = document.querySelector('input[name ="xAxisScale"]:checked').value;
-                        let referenceTime = stringObj.referenceTime;
-                        let fileName = 'WF_by_' + xAxisName + (xAxisScale == 'band' ? '-sta' : '') + '_' + referenceTime + 'Z';
-                        downloadSvg(svgArr, fileName, option);
-                    });
-
-                    li.append(item);
-                    ul.append(li);
-                });
-                document.querySelector('#charts').append(div);
-                document.querySelector('#chart' + i).append(nav);
-            };
-            let MenuEvents = () => {
-                let charts = document.getElementById('charts');
-                let stopPropagation = (e) => {
-                    e.stopPropagation();
-                }
-
-                //start or stop DOM event capturing
-                function chartEventControl(control) {
-                    if (control == 'stop') {
-                        // console.debug('add');
-                        charts.addEventListener('mousemove', stopPropagation, true);
-                        charts.addEventListener('mouseenter', stopPropagation, true);
-                    }
-                    else {
-                        // console.debug('remove');
-                        charts.removeEventListener('mousemove', stopPropagation, true);
-                        charts.removeEventListener('mouseenter', stopPropagation, true);
-                    }
-                }
-
-                chartContainerJQ.find('.toggle-nav').off('click');
-                chartContainerJQ.find('.toggle-nav').click(function (e) {
-                    // console.debug(e.target === this);//e.target===this
-
-                    $(this).toggleClass('active');
-                    $(this).next().toggleClass('active');
-                    e.preventDefault();
-
-                    //選單打開後阻止事件Capture到SVG(選單打開後svg反應mousemove,mouseenter圖片會有問題)
-                    if ($(this).hasClass('active'))
-                        chartEventControl('stop');
-                    else
-                        chartEventControl('start');
-
-
-                });
-                // console.debug(chartContainerJQ.find(".toggle-nav"));
-                $('body').off('click');
-                $('body').click(function (e) {
-                    chartContainerJQ.find(".toggle-nav").each((i, d) => {
-                        // console.debug(e.target == d);
-                        // console.debug(e.target);
-                        if (e.target != d && $(d).hasClass('active')) {
-                            $(d).toggleClass('active');
-                            $(d).next().toggleClass('active');
-
-                            setTimeout(() => chartEventControl('start'), 100);
-                        }
-                    });
-                });
-            };
-            let downloadSvg = (svgArr, fileName, option) => {
-
-                function getSvgUrl(svgNode) {
-                    let svgData = (new XMLSerializer()).serializeToString(svgNode);
-                    let svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
-                    let svgUrl = URL.createObjectURL(svgBlob);
-                    return svgUrl;
-                }
-                function getCanvas(resize) {
-                    // =============== canvas init
-                    let canvas = document.createElement('canvas');
-                    let context = canvas.getContext('2d');
-
-                    let svgWidth = svgArr[0].viewBox.baseVal.width;
-                    let svgHeight = svgArr[0].viewBox.baseVal.height * svgArr.length;
-                    let canvasWidth, canvasHeight;
-                    //檢視時縮放,下載時放大
-                    if (resize) {
-                        let windowW = window.innerWidth;//获取当前窗口宽度 
-                        let windowH = window.innerHeight;//获取当前窗口高度 
-
-                        let width, height;
-                        let scale = 0.9;//缩放尺寸
-                        height = windowH * scale;
-                        width = height / svgHeight * svgWidth;
-                        while (width > windowW * scale) {//如宽度扔大于窗口宽度 
-                            height = height * scale;//再对宽度进行缩放
-                            width = width * scale;
-                        }
-                        canvasWidth = width;
-                        canvasHeight = height;
-                    }
-                    else {
-                        let scale = 1.5;
-                        canvasWidth = svgWidth * scale;
-                        canvasHeight = svgHeight * scale;
-                    }
-
-                    canvas.width = canvasWidth;
-                    canvas.height = canvasHeight;
-                    //====bgcolor
-                    context.fillStyle = "white";
-                    context.fillRect(0, 0, canvas.width, canvas.height);
-                    return [canvas, context];
-
-                }
-                function download(href, name) {
-                    let downloadLink = document.createElement("a");
-                    downloadLink.href = href;
-                    downloadLink.download = name;
-                    document.body.appendChild(downloadLink);
-                    downloadLink.click();
-                    document.body.removeChild(downloadLink);
-                }
-                function show(width, height) {
-                    // $('#bigimg').attr("src", img);//设置#bigimg元素的src属性 
-                    // $('#outerdiv').fadeIn("fast");//淡入显示#outerdiv及.pimg 
-                    // $('#outerdiv').off('click');
-                    // $('#outerdiv').click(function () {//再次点击淡出消失弹出层 
-                    //     $(this).fadeOut("fast");
-                    // });
-                    let outerdiv = $('#outerdiv');
-
-                    outerdiv.fadeIn("fast");//淡入显示#outerdiv及.pimg 
-                    outerdiv.off('click');
-                    outerdiv.click(function (e) {//再次点击淡出消失弹出层 
-                        if (e.target.id != 'outerdiv') return;
-                        $(this).fadeOut("fast");
-                        $(originParent).children('svg').remove();
-                        originSvg.removeAttribute('width');
-                        originSvg.removeAttribute('height');
-                        originParent.append(originSvg);
-                    });
-
-                    let originSvg = svgArr[0];
-                    let originParent = originSvg.parentNode;
-                    let cloneSvg = originSvg.cloneNode(true);
-                    originSvg.setAttribute('width', width);
-                    originSvg.setAttribute('height', height);
-                    document.querySelector('#innerdiv').append(originSvg);
-                    originParent.append(cloneSvg);
-
-                }
-
-
-                if (option == 'svg') {
-                    //==============merge svg
-                    let newSvg = document.createElement('svg');
-
-
-                    svgArr.forEach(queryStr => {
-                        let svgjQobj = $(queryStr);
-                        svgjQobj.clone().appendTo(newSvg);
-                    });
-                    // console.debug(newSvg);
-                    let svgUrl = getSvgUrl(newSvg);
-                    download(svgUrl, fileName + '.' + option);
-                }
-                else {
-                    //==============each svg draw to canvas
-                    let CanvasObjArr = getCanvas(option == 'bigimg');
-
-                    let canvas = CanvasObjArr[0];
-                    let context = CanvasObjArr[1];
-                    let imageWidth = canvas.width;
-                    let imageHeight = canvas.height / svgArr.length;
-
-
-                    svgArr.forEach((queryStr, index) => {
-                        let svgNode = $(queryStr)[0];
-                        let svgUrl = getSvgUrl(svgNode);
-                        let image = new Image();
-                        image.src = svgUrl;
-                        image.onload = () => {
-                            context.drawImage(image, 0, index * imageHeight, imageWidth, imageHeight);
-
-                            //done drawing and output
-                            if (index == svgArr.length - 1) {
-                                let imgUrl;
-                                if (option == 'bigimg') {
-                                    show(imageWidth, imageHeight);
-                                }
-                                else {
-                                    imgUrl = canvas.toDataURL('image/' + option);
-                                    download(imgUrl, fileName + '.' + option);
-                                }
-                            }
-                        }
-                    });
-                }
-
-            };
+            selectorD3.selectAll('#Map>*').remove();
 
             function animeMap() {
                 console.debug(data);
-
-                let Map = L.map('Map', {
-                    attributionControl: false,
-                    zoomControl: false
-                });
-                let markerGroup = new L.layerGroup();
 
                 const getSize = (ML) => {
                     let ml_base = 3,
@@ -336,14 +96,29 @@ function TWSanime() {
                     return ML > ml_base ?
                         (ML - ml_base) * circleSize + 0.1 : 0.1;
                 };
+
+                let Map = L.map('Map', {
+                    attributionControl: false,
+                    zoomControl: false
+                });
+                let markerGroup = new L.layerGroup();
+
+                const minSpeed = 5 * 86400000;//==5d/s
+                const totalTime = (data[data.length - 1].date - data[0].date) / minSpeed;
+
                 const
                     mlDomain = [3, 7],
                     maxDepth = 320;
                 const depthScale = d3.scaleSequentialSqrt([maxDepth, 0], d3.interpolateTurbo);
+                const timeScale = d3.scaleTime()
+                    .domain([data[0].date, data[data.length - 1].date])
+                    .range([0, totalTime]);
 
-                const minSpeed = 5 * 86400000;//==5d/s
-                const totalFrames = (data[data.length - 1].date - data[0].date) / minSpeed;
-                let keyframe;
+
+                const mapSelectorD3 = selectorD3.selectAll('#Map');
+                const dateText = mapSelectorD3.append('span').attr("class", "dateText");
+                const setting = mapSelectorD3.append('div').attr("class", "setting");
+
                 function init() {
                     let initMap = () => {
                         const tileProviders = [
@@ -417,23 +192,73 @@ function TWSanime() {
                             markerGroup.addLayer(marker);
 
                         });
-                    }
+                    };
+                    let initToolBar = () => {
+                        const icons = ['setting', 'pause',];
+
+                        let buttonHtml = icons.map(btn => `
+                            <div class="toolButton">
+                                <span class="tooltiptext tooltip-top">${btn}</span>
+                                <a class="button" id="${btn}Btn" href="${btn === 'setting' ? '#setPanel' : '#'}"></a>
+                            </div>`).join('');
+
+                        let setPanelHtml = `
+                        <div id="setPanel" class="popup">
+                            <h2>Here i am</h2>
+                            <a class="close" href="#">&times;</a>
+                            <div class="content">
+                            Thank to pop me out of that button, but now i'm done so you can close this window.
+                            </div>
+                        </div>`;
+
+                        setting.node().insertAdjacentHTML('beforeend',
+                            `<div class="toolBar d-flex flex-row">
+                                ${buttonHtml}
+                            </div>`);
+                        icons.forEach(btn => {
+                            let button = setting.selectAll(`#${btn}Btn`);
+                            button
+                                .style('content', `url(img/${btn}.png)`);
+
+                            if (btn === 'setting')
+                                button.node().parentNode.insertAdjacentHTML('beforeend', setPanelHtml);
+                        });
+                    };
+
+
                     initMap();
                     initMarker();
+                    initToolBar();
+
                     markerGroup.addTo(Map);
                 };
                 function update() {
+                    let updateDateStr = () => {
+                        dateText.transition()
+                            .ease(d3.easeLinear)
+                            .duration(timeScale.range()[1] * 1000)
+                            .tween("date", () => {
+                                let i = d3.interpolateDate(...timeScale.domain());
+                                return t =>
+                                    dateText.text(d3.timeMinute(i(t)).toISOString().substring(0, 16));
+                            });
+                    };
                     let updateMarker = (data) => {
                         // markerGroup.clearLayers();
 
                         // marker.getElement().style.display = 'inline';
                         let markers = markerGroup.getLayers();
                         data.forEach((d, i) => {
-                            markers[i].getElement().style.display = 'inline';
+                            // console.debug(timeScale(d.date))
+                            d3.timeout(() => {
+                                markers[i].getElement().style.display = 'inline';
+
+                            }, timeScale(d.date) * 1000);
                         });
 
                     };
                     updateMarker(data);
+                    updateDateStr();
                 };
 
                 init();
@@ -451,7 +276,7 @@ function TWSanime() {
             animeMap();
         };
         //===init once
-        if (!(chartContainerJQ.find('#form-chart').length >= 1)) {
+        if (!(selectorD3.selectAll('#form-chart').nodes().length >= 1)) {
             init();
         };
 
